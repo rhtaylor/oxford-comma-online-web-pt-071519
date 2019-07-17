3eb50edbc313bf7b20a2f143dc33df3367a985a2
Randall Taylor <randyisbusy@gmail.com>
2019-07-17 19:37:13 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,9 @@ def oxford_comma(array)
   if array.size == 2
   two = array.join(" and ")
   two
-elsif
+elsif array.size > 2
+  index = array.size - 1
+  comma = array.insert(index, " and ")
   comma = array.join(" , ")
   comma
 end
