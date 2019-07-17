d3d6c1764b0cd5c58d6549e4d4f51e95bc3d6900
Randall Taylor <randyisbusy@gmail.com>
2019-07-17 20:02:30 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -10,7 +10,7 @@ elsif array.size > 2
   comma = array.pop
   comma1 = array.join(", ")
   slick = save.to_s
-  comma2 = comma1 << " and "
+  comma2 = comma1 << ", and "
   comma3 = comma2 << slick
   comma3
 end
