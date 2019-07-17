79dae64a9ad90f2f11190aea56d22a732570fa61
Randall Taylor <randyisbusy@gmail.com>
2019-07-17 20:05:29 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    return array[0]
+  elsif   array.size == 2
+  two = array.join(" and ")
+  two
+elsif array.size > 2
+  index = array.size - 1
+  save = array[index]
+  comma = array.pop
+  comma1 = array.join(", ")
+  slick = save.to_s
+  comma2 = comma1 << ", and "
+  comma3 = comma2 << slick
+  comma3
+end
 end
