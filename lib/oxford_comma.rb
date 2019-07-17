d58abe0f8568d63568e88c718e23b121d93094e0
Randall Taylor <randyisbusy@gmail.com>
2019-07-17 19:51:24 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,9 +6,9 @@ def oxford_comma(array)
   two
 elsif array.size > 2
   index = array.size - 1
-  
+
   comma = array.join(" , ")
-  slick = comma[-1] 
+  slick = comma[-1]
   hm = slick.unshift(" , ")
   comma.pop.join(comma.length, hm)
 end
